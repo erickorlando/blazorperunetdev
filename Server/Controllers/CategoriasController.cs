95854e3ffc5ad06679505e9f71e7eb59c59ea262
Erick Orlando <erickorlando@outlook.com>
2023-10-03 22:09:00 -0500

    Add project files.

---
+++ Server/Controllers/CategoriasController.cs
@@ -0,0 +1,27 @@
+﻿using ECommerceWeb.Server.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceWeb.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoriasController : ControllerBase
+{
+    private readonly List<Categoria> _list;
+
+    public CategoriasController()
+    {
+        _list = new List<Categoria>()
+        {
+            new() { Id = 1, Nombre = "Celulares"},
+            new() { Id = 2, Nombre = "Televisores"},
+            new() { Id = 3, Nombre = "Electrodomésticos"},
+        };
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(await Task.FromResult(_list)); //Carga la lista inicializada
+    }
+}
---
+++ Server/Entities/Categoria.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerceWeb.Server.Entities;
+
+public class Categoria
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = default!;
+}
