f0cf425be4c896337d17dbe1b90f9b5137bee92e
Erick Orlando <erickorlando@outlook.com>
2023-10-17 22:07:49 -0500

    Configuracion de sesion en el frontend

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,5 @@
+using Blazored.LocalStorage;
+using Blazored.SessionStorage;
 using Blazored.Toast;
 using CurrieTechnologies.Razor.SweetAlert2;
 using ECommerceWeb.Client;
@@ -8,9 +10,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddBlazoredToast();
 builder.Services.AddSweetAlert2();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredSessionStorage();
 
 await builder.Build().RunAsync();
