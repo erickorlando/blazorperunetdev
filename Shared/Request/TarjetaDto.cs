fcaa2080569bf08647ed2334308f6c253611cfec
Erick Orlando <erickorlando@outlook.com>
2023-10-26 20:21:52 -0500

    Carrito de compra

---
+++ Shared/Request/TarjetaDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerceWeb.Shared.Request;
+
+public class TarjetaDto
+{
+    [Required(ErrorMessage = "Ingrese Titular")]
+    public string? Titular { get; set; }
+
+    [RegularExpression(@"^\d{4}-\d{4}-\d{4}-\d{4}$", ErrorMessage = "Ingrese una tarjeta valida")]
+    public string? Numero { get; set; }
+
+    [Required(ErrorMessage = "Ingrese Vigencia")]
+    [MaxLength(5)]
+    public string? Vigencia { get; set; }
+
+    [Required(ErrorMessage = "Ingrese CVV")]
+    [MaxLength(3)]
+    public string? Cvv { get; set; }
+}
